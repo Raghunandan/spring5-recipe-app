3df589be67bd09480e6dac280d5414ffa68d9d80
Raghunandan R <raghunan@gmail.com>
2020-11-11 19:55:00 +0530

    Added initial models

---
+++ src/main/java/guru/springframework/model/Ingredient.java
@@ -0,0 +1,60 @@
+package guru.springframework.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.EAGER) //this is default for one to one
+    private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Receipe receipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Receipe getReceipe() {
+        return receipe;
+    }
+
+    public void setReceipe(Receipe receipe) {
+        this.receipe = receipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
+}
---
+++ src/main/java/guru/springframework/model/Notes.java
@@ -0,0 +1,41 @@
+package guru.springframework.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Receipe receipe;
+
+    @Lob
+    private String receipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Receipe getReceipe() {
+        return receipe;
+    }
+
+    public void setReceipe(Receipe receipe) {
+        this.receipe = receipe;
+    }
+
+    public String getReceipeNotes() {
+        return receipeNotes;
+    }
+
+    public void setReceipeNotes(String receipeNotes) {
+        this.receipeNotes = receipeNotes;
+    }
+}
